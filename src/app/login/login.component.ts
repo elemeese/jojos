db5d3b6983f9983a81e06c4fb021439df6864c66
elemeese <164366962+elemeese@users.noreply.github.com>
2024-09-12 22:32:59 -0300

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,3 @@
-// login.component.ts
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
 
@@ -11,12 +10,19 @@ export class LoginComponent {
   username: string = '';
   password: string = '';
   showPassword: boolean = false;
+  loginError: string = '';
 
   constructor(private navCtrl: NavController) {}
 
   login() {
-    // Aquí iría la lógica de autenticación
-    this.navCtrl.navigateForward('/home');
+    // Lógica de autenticación básica
+    if (this.username === 'alumno' && this.password === 'password123') {
+      // Si las credenciales son correctas, navega a la página de inicio
+      this.navCtrl.navigateForward('/home');
+    } else {
+      // Si las credenciales no son correctas, muestra un mensaje de error
+      this.loginError = 'Nombre de usuario o contraseña incorrectos.';
+    }
   }
 
   goToResetPassword() {
